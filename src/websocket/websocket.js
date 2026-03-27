e39fd22743695373ed9eb41836b3e3c9b8e96277
Malavika Ranjit <malavikaranjit27@gmail.com>
2026-03-27 22:12:34 +0530

    Changes

--- src/websocket/websocket.js
+++ src/websocket/websocket.js
@@ -1,29 +1,30 @@
 import { useDispatch } from "react-redux";
 import { updateProgress, updateReport } from "../slice/progressSlice";
-// import { useRef } from "react";
-let socket = null;   
+import { useRef } from "react";
+// let socket = null;   
 
 export const useWebSocketTask = (deviceId,handleScanCompleted) => {
-    // const wsRef = useRef(null);
+    const wsRef = useRef(null);
     const dispatch = useDispatch();
 
     const startTask = () => {
         // const ws = new WebSocket(`ws://10.107.190.46:8000/ws/frontend/${deviceId}`);
         // wsRef.current = ws;
-        if (socket) {
+        if (wsRef.current) {
     console.log("Socket already running");
     return;
 }
 
-socket = new WebSocket(`ws://10.107.190.46:8000/ws/frontend/${deviceId}`);
+const ws= new WebSocket(`ws://localhost:8000/ws/frontend/${deviceId}`);
+wsRef.current=ws;
 
 
-        socket.onopen = () => {
+        ws.onopen = () => {
             console.log("Connected");
             ws.send(JSON.stringify({ event: "START_SCAN" })); // FIX: stringify
         };
 
-        socket.onmessage = (event) => {
+        ws.onmessage = (event) => {
             const data = JSON.parse(event.data);
             console.log("message from backend:",data)
             if (data.event === "SCAN_PROGRESS") {
@@ -33,7 +34,7 @@ socket = new WebSocket(`ws://10.107.190.46:8000/ws/frontend/${deviceId}`);
 
             if (data.event === "SCAN_COMPLETED") {
                 handleScanCompleted(); //closure to handle completion
-                console.log("SCAN COMPLETED")
+                // console.log("SCAN COMPLETED")
             }
 
             if (data.event == "FILE_COUNT"){
@@ -50,7 +51,7 @@ socket = new WebSocket(`ws://10.107.190.46:8000/ws/frontend/${deviceId}`);
                     high: high
                 }))
 
-                socket.close();
+                ws.close();
             }   
         };
 
@@ -58,7 +59,7 @@ socket = new WebSocket(`ws://10.107.190.46:8000/ws/frontend/${deviceId}`);
             console.log("Disconnected");
         };
 
-        socket.onerror = (err) => {
+        ws.onerror = (err) => {
             console.error(err);
         };
     };
